eb60233f12f3239cbd0ccea631fbc0c23a9bbf7d
Naafiul Razzaq <naafiulrazzaq@gmail.com>
2023-10-31 15:52:46 +0700

    second commit

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -27,7 +27,7 @@ const Projects = () => {
               </div>
               <div >
               <button type="button" class="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
-              onClick={() => navigate('form-pendaftaran')}>Formulir Keanggotaan</button>
+              onClick={() => navigate('/form-pendaftaran')}>Formulir Keanggotaan</button>
               </div>
               <div className="">
               <button type="button" class="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
